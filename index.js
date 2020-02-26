1148b3529ec2854c04af6bd85efed9df0df5af91
ocy <zichengou@gmail.com>
2020-02-26 15:37:27 +0800

    quality: quality

--- index.js
+++ index.js
@@ -73,9 +73,13 @@ const init = async () => {
             const quality = id === query.a ? "highestaudio" : "highestvideo";
             const ContentType = id == query.a ? 'audio/mp3' : 'video/mp4';
             const requestRange = request.headers.range;
+            console.log('query', query);
+            console.log('id', id);
+            console.log('quality', quality);
+            console.log('contentype', ContentType);
             try {
                 // 请求视频内容
-                const media = ytdl(id, { highWaterMark: 1 << 25, quality });
+                const media = ytdl(id, { highWaterMark: 1 << 25, quality: quality });
                 // 返回视频流
                 if (requestRange) {
                     const p = new Promise((res, rej) => {
